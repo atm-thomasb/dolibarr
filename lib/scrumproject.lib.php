2117729b289c52f50ef9b0d73e15cff4b52161ac
Maxime Kohlhaas <maxime@atm-consulting.fr>
2020-12-25 11:59:19 +0100

    Add extrafield management

--- lib/scrumproject.lib.php
+++ lib/scrumproject.lib.php
@@ -40,12 +40,10 @@ function scrumprojectAdminPrepareHead()
 	$head[$h][2] = 'settings';
 	$h++;
 
-	/*
-	$head[$h][0] = dol_buildpath("/scrumproject/admin/myobject_extrafields.php", 1);
+	$head[$h][0] = dol_buildpath("/scrumproject/admin/scrumsprint_extrafields.php", 1);
 	$head[$h][1] = $langs->trans("ExtraFields");
 	$head[$h][2] = 'myobject_extrafields';
 	$h++;
-	*/
 
 	$head[$h][0] = dol_buildpath("/scrumproject/admin/about.php", 1);
 	$head[$h][1] = $langs->trans("About");
