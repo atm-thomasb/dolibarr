551176c07a80063bffc1c5b811afac5728076437
John BOTELLA <john.botella@atm-consulting.fr>
2022-08-05 15:42:36 +0200

    Update class/scrumuserstory.class.php

--- class/scrumuserstory.class.php
+++ class/scrumuserstory.class.php
@@ -43,7 +43,7 @@ class ScrumUserStory extends CommonObject
 	/**
 	 * @var string ID to identify managed object.
 	 */
-	public $element = 'scrumuserstory';
+	public $element = 'scrumproject_scrumuserstory';
 
 	/**
 	 * @var string Name of table without prefix where object is stored. This is also the key used for extrafields management.
