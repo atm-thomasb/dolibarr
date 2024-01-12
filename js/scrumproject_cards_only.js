57485f5215142bb402e541f708e45b86d61ce888
John BOTELLA <john.botella@atm-consulting.fr>
2024-01-12 11:34:34 +0100

    Fix button double click send multi email

--- js/scrumproject_cards_only.js
+++ js/scrumproject_cards_only.js
@@ -2,13 +2,13 @@ jQuery(function ($) {
 
 	// Disable double click on some elements and add loading icon
 	$('body').on('click', '#sendmail', function(event) {
-		event.target.setAttribute('aria-busy', 'true');
 		if ($(this).attr('aria-busy') == true){
 			console.log('Double click blocked by scrumproject_card_only.js');
 			event.preventDefault();
 			return;
 		}
+
+		event.target.setAttribute('aria-busy', 'true');
+
 	});
-
-
 });
